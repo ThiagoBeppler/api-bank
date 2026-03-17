d73dd95494b4d61b7d66f2549a3936de6cda69e5
Thiago <thiagobeppler@hotmail.com>
2026-03-17 16:56:44 -0300

    Reset return adjustment

--- src/main/java/com/example/apibank/controllers/AccountController.java
+++ src/main/java/com/example/apibank/controllers/AccountController.java
@@ -8,6 +8,7 @@ import com.example.apibank.interfaces.AccountService;
 import com.example.apibank.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -56,11 +57,9 @@ public class AccountController {
         return accountService.transferEvent(event);
     }
 
-    @PostMapping("reset")
-    public HttpStatus reset(){
-
+    @PostMapping("/reset")
+    public ResponseEntity<String> reset() {
         accountService.reset();
-
-        return HttpStatus.OK;
+        return ResponseEntity.ok("OK");
     }
 }
